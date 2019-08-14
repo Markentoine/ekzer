67fdd77ef5244367a1dc0b9ad0448408e44f1dfc
Jerome Toulouse <monsieur.toulouse@gmail.com>
2019-08-14 07:57:58 +0200

    Add aliases Insertion.

--- lib/ekzer/insertion.ex
+++ lib/ekzer/insertion.ex
@@ -1,4 +1,5 @@
 defmodule Ekzer.Insertion do
+    alias Ekzer.{Repo, Exercice}
     def create_exercise(level, type, field) do
         {:ok, exercice} = Repo.insert(%Exercice{level: level, type: type, field: field})
         exercice
--- lib/ekzer/interface.ex
+++ lib/ekzer/interface.ex
@@ -1,5 +1,5 @@
 defmodule Ekzer.Interface do
-    alias Ekzer.{Repo, Exercice, Insertion}
+    alias Ekzer.{Insertion}
 
     def create(level, type, field) do
         Insertion.create_exercise(level, type, field)
