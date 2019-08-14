eb764ee2bc90ca2d7656182ec3b9baad48086caf
Jerome Toulouse <monsieur.toulouse@gmail.com>
2019-08-14 05:57:19 +0200

    interface.

--- lib/ekzer.ex
+++ lib/ekzer.ex
@@ -1,18 +1,3 @@
 defmodule Ekzer do
-  @moduledoc """
-  Documentation for Ekzer.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Ekzer.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  
 end
---
+++ lib/ekzer/interface.ex
@@ -0,0 +1,3 @@
+defmodule Ekzer.Interface do
+    
+end
